c35b01a9995d0cef1cb451822f740db6ce0bfe92
QuocHuy71219 <huyTNHtin1314@gmail.com>
2021-10-20 10:13:17 +0700

    lab05

---
+++ Lap5_DB4O/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lap5_DB4O
+{
+    public class Employee
+    {
+        public int Ssn { get; set; }
+        public string FName { get; set; }
+        public char MInit { get; set; }
+        public string LName { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        public float Salary { get; set; }
+        public char Gender { get; set; }
+        // relationships
+        public List<WorksOn> WorksOn { get; set; }
+        public Department WorksFor { get; set; }
+        public Department Manages { get; set; }
+        public Employee Supervisor { get; set; }
+        public List<Employee> Supervisees { get; set; }
+        public List<Dependent> Dependents { get; set; }
+    }
+}
---
+++ Lap5_DB4O/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lap5_DB4O
+{
+    public class Project
+    {
+        public int PNumber { get; set; }
+        public string PName { get; set; }
+        public string Location { get; set; }
+        // relationships
+        public Department ControlledBy { get; set; }
+        public List<WorksOn> WorksOn { get; set; }
+    }
+    public class WorksOn
+    {
+        // attribute
+        public float Hours { get; set; }
+        //owner attributes
+        public Employee Employee { get; set; }
+        public Project Project { get; set; }
+    }
+}
